f5374997967b4b7e702e38070de6556125cf9bd5
Ethan <71776311+EvasiveAce@users.noreply.github.com>
2023-03-20 23:33:40 -0400

    Add files via upload

---
+++ Warehouse/Classes/Crate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Classes
+{
+    public class Crate
+    {
+        string Id { get; set; }
+        double Price { get; set; } = new Random().Next(50, 501);
+    }
+}
---
+++ Warehouse/Classes/Truck.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.Classes
+{
+    public class Truck
+    {
+        string Driver { get; set; }
+        string DeliveryCompany { get; set; }
+        Stack<Crate> Trailer { get; set; }
+
+        public void Load(Crate crate)
+        {
+            Trailer.Push(crate);
+        }
+
+        public Crate Unload()
+        {
+            return Trailer.Pop();
+        }
+    }
+}
